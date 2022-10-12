b40a1926746479adec94d5d9607f3e0719504686
Melk de Sousa <melk.sousap@gmail.com>
2022-10-12 06:41:13 -0300

    define tokens of font size and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,25 @@ module.exports = {
     './src/**/*.css'
   ],
   theme: {
+    fontSize: {
+      xs:14,
+      sm:16,
+      md: 18,
+      lg:20,
+      xl:24,
+      '2xl': 32
+    },
+
+    colors:{
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7c7c8a',
+      'gray-200': '#c4c4cc',
+      'gray-100': '#e1e1e6',
+
+      'cyan-500': '#81d8f7',
+      'gray-300': '#9be1fb',
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
