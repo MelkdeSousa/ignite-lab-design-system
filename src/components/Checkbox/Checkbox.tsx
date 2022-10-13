c4898ff48b43d622c04263185684804928b4ffb5
Melk de Sousa <melk.sousap@gmail.com>
2022-10-13 08:19:02 -0300

    feat: extends props of Checkbox of radix

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -2,11 +2,11 @@ import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
 import clsx from 'clsx'
 import { Check } from 'phosphor-react'
 
-export interface CheckboxProps { }
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps { }
 
-export const Checkbox = (props: CheckboxProps) => {
+export const Checkbox = ({ className, ...props }: CheckboxProps) => {
     return (
-        <CheckboxPrimitive.Root className={clsx('w-6 h-6 p-[2px] bg-gray-800 rounded')}>
+        <CheckboxPrimitive.Root className={clsx('w-6 h-6 p-[2px] bg-gray-800 rounded', className)} {...props}>
             <CheckboxPrimitive.Indicator asChild>
                 <Check weight="bold" className={clsx('h-5 w-5 text-cyan-500')} />
             </CheckboxPrimitive.Indicator>
